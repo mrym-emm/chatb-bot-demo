b09e5a847458e43dc07d8778b41779c389ab3f26
mrym-emm <153648180+mrym-emm@users.noreply.github.com>
2025-07-17 23:30:10 +0800

    Update main.py

--- main.py
+++ main.py
@@ -1,4 +1,55 @@
+import os
 import streamlit as st
-import groq
+from groq import Groq
+from dotenv import load_dotenv
 
-st.title("Hi")
+load_dotenv()
+# streamlit page configuration
+st.set_page_config(
+    page_title="LLAMA 3.1. ChatBot",
+    layout="centered"
+)
+
+client = Groq(
+    api_key=os.getenv("GROQ_API"),
+)
+
+# initialize the chat history as streamlit session state of not present already
+if "chat_history" not in st.session_state:
+    st.session_state.chat_history = []
+
+
+# streamlit page title
+st.title("Chatbot with Groq API -LLAMA 3.1. Chat")
+
+# display chat history
+for message in st.session_state.chat_history:
+    with st.chat_message(message["role"]):
+        st.markdown(message["content"])
+
+
+# input field for user's message:
+user_prompt = st.chat_input("Ask LLAMA...")
+
+if user_prompt:
+
+    st.chat_message("user").markdown(user_prompt)
+    st.session_state.chat_history.append({"role": "user", "content": user_prompt})
+
+    # sens user's message to the LLM and get a response
+    messages = [
+        {"role": "system", "content": "You are a helpful assistant"},
+        *st.session_state.chat_history
+    ]
+
+    response = client.chat.completions.create(
+        model="llama-3.3-70b-versatile",
+        messages=messages
+    )
+
+    assistant_response = response.choices[0].message.content
+    st.session_state.chat_history.append({"role": "assistant", "content": assistant_response})
+
+    # display the LLM's response
+    with st.chat_message("assistant"):
+        st.markdown(assistant_response)
